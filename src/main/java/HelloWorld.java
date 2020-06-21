6d7e644ffec5e9fa3ffd0c8eeddd6e3fda04dc3f
gauravkumar9130 <55126211+gauravkumar9130@users.noreply.github.com>
2020-06-21 15:15:05 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
-package helloworld;
-public class HelloWorld {
-	public static void main(String args[]) {
-		System.out.println("Hello World, Maven");
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
